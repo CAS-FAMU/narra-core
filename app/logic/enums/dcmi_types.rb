6fdb50adb04159b06291108a32ae588a15d332d2
K0F <krystof.pesek@gmail.com>
2013-06-09 18:35:37 +0200

    Issue #4: DCMI Types Vocabulary added

---
+++ app/logic/enums/dcmi_types.rb
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2013 CAS / FAMU
+#
+# This file is part of Narra Core.
+#
+# Narra Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Narra Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Narra Core. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Michal Mocnak <michal@marigan.net>, Krystof Pesek <krystof.pesek@gmail.com>
+#
+
+# Basic DCMI TYPES VOCABULARY implementation
+# http://dublincore.org/documents/2012/06/14/dcmi-terms/?v=elements#contributor
+
+module DcmiTypes
+
+  COLLECTION = 0
+  DATASET = 1
+  EVENT = 2
+  IMAGE = 3
+  INTERACTIVE_RESOURCE = 4
+  MOVING_IMAGE = 5
+  PHYSICAL_OBJECT = 6
+  SERVICE = 7
+  SOFTWARE = 8
+  SOUND = 9
+  STILL_IMAGE = 10
+  TEXT = 11
+
+end
