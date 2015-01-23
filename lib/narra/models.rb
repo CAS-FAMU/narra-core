c02811fbf161ba372c93aeb607cefab11a849f83
marigan <michal@marigan.net>
2015-01-23 15:27:41 +0100

    Narra models dependency requirement for plugins

---
+++ lib/narra/models.rb
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2014 CAS / FAMU
+#
+# This file is part of Narra Core.
+#
+# Narra Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Narra Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Narra Core. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Michal Mocnak <michal@marigan.net>
+#
+
+require 'narra/audio'
+require 'narra/cache'
+require 'narra/event'
+require 'narra/identity'
+require 'narra/image'
+require 'narra/item'
+require 'narra/junction'
+require 'narra/library'
+require 'narra/mark'
+require 'narra/meta'
+require 'narra/project'
+require 'narra/sequence'
+require 'narra/user'
+require 'narra/video'
+
+module Narra
+  module Models
+
+  end
+end
