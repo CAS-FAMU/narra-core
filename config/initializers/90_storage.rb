6e7b7421835d787721ac2f9d7c78ab62c6cde88d
marigan <michal@marigan.net>
2015-09-05 15:23:35 +0200

    Google storage implemented

--- config/initializers/90_storage.rb
+++ config/initializers/90_storage.rb
@@ -52,11 +52,22 @@ case Narra::Storage::TYPE
       config.storage = :fog
       config.fog_credentials = {
           provider: 'AWS',
-          aws_access_key_id: ENV['NARRA_AWS_ACCESS_KEY'],
-          aws_secret_access_key: ENV['NARRA_AWS_SECRET'],
-          region: ENV['NARRA_AWS_REGION'],
+          aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+          aws_secret_access_key: ENV['AWS_SECRET'],
+          region: ENV['AWS_REGION'],
       }
-      config.fog_directory = ENV['NARRA_AWS_BUCKET']
+      config.fog_directory = ENV['AWS_BUCKET']
+      config.cache_dir = Narra::Tools::Settings.storage_temp
+    end
+  when :google
+    CarrierWave.configure do |config|
+      config.fog_provider = :fog-google
+      config.fog_credentials = {
+          provider: 'Google',
+          google_storage_access_key_id: ENV['GOOGLE_CLIENT_ID'],
+          google_storage_secret_access_key: ENV['GOOGLE_CLIENT_SECRET']
+      }
+      config.fog_directory = ENV['GOOGLE_BUCKET']
       config.cache_dir = Narra::Tools::Settings.storage_temp
     end
 end
