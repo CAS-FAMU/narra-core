14a015b2cf837f9b9d235f7d8858de5adf155ba1
Michal Mocnak <michal@marigan.net>
2017-07-31 10:11:02 +0400

    spec users support modified

--- spec/support/users.rb
+++ spec/support/users.rb
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Narra Core. If not, see <http://www.gnu.org/licenses/>.
 #
-# Authors: Michal Mocnak <michal@marigan.net>, Krystof Pesek <krystof.pesek@gmail.com>
+# Authors: Michal Mocnak <michal@marigan.net>
 #
 
 RSpec.configure do |config|
@@ -33,16 +33,12 @@ RSpec.configure do |config|
     Narra::Identity.create_from_hash(contributor_hash)
     Narra::Identity.create_from_hash(unroled_hash)
     # get admin token and user
-    @admin_token = Base64.urlsafe_encode64(admin_hash[:uid])
     @admin_user = Narra::User.find_by(name: 'Admin')
     # get author token and user
-    @author_token = Base64.urlsafe_encode64(author_hash[:uid])
     @author_user = Narra::User.find_by(name: 'Author')
     # get contributor token and user
-    @contributor_token = Base64.urlsafe_encode64(contributor_hash[:uid])
     @contributor_user = Narra::User.find_by(name: 'Contributor')
     # get guest token and user
-    @unroled_token = Base64.urlsafe_encode64(unroled_hash[:uid])
     @unroled_user = Narra::User.find_by(name: 'Unroled')
     @unroled_user.roles = []
     @unroled_user.save
